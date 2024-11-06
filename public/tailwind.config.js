e7d2a01a2df66a3249ae4c98017029cd470a5c90
dahouzfatima <dahouzfatima23@gmail.com>
2024-11-06 23:40:13 +0100

    first commit

---
+++ public/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            customGreen: '#0C9D79', // Vous pouvez nommer cette couleur comme vous le souhaitez
+          },
+      },
+    },
+    plugins: [],
+  }
